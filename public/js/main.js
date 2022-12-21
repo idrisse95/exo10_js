fc9d3c127808eb7dc6cb057357b0fbd0d3d9cb2c
55973 <55973@etu.he2b.be>
2022-12-21 11:46:26 +0100

    un

---
+++ public/js/main.js
@@ -0,0 +1,13 @@
+for (let un = 0; un < 20; un++) {
+   
+    if (un % 3 != 0) { 
+        console.log(un);
+    }
+
+    else {
+        console.log(un +"ceci es un multiple de 3");
+    }
+
+    
+    
+}
