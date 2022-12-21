a438dd78fa2d40efd5b2a196418d638444e3e90e
55973 <55973@etu.he2b.be>
2022-12-21 12:12:14 +0100

    cind fin

--- public/js/main.js
+++ public/js/main.js
@@ -18,7 +18,7 @@ for (let un = 0; un < 20; un++) {
 
 // do {  
 
-//     deux = +prompt('entre nb paire')
+//     deux = +prompt('entre nb imapire')
     
 // } while (deux % 2 != 0); 
 
@@ -27,16 +27,58 @@ for (let un = 0; un < 20; un++) {
 // trois
 
 
-for (let trois = 0; trois < 10;trois++) {
+// for (let trois = 0; trois < 10;trois++) {
  
-    console.log(trois);
+//     console.log(trois);
 
-switch (true) {
-    case trois==7:
-        alert('stop')
-        break;
+// switch (true) {
+//     case trois==7:
+//         alert('stop')
+//         break;
 
-    default:
-        break;
-}}
+//     default:
+//         break;
+// }}
+
+
+
+
+
+// quatre
+
+console.log(' ');
+for (let imapire = 0; imapire < 11; imapire++) {
+    if (imapire % 2 != 0) {
+        console.log(imapire);
+        
+    }
+    
+    
+}
+
+
+
+console.log('');
+
+// cinq
+
+for (let cinq = 0; cinq < 10; cinq++) {
+   
+    switch (true) {
+        case cinq < 6:
+            console.log(cinq);
+          
+            break;
+       
+        default:
+            console.log(('interompue'));
+
+            break;
+    }
+    
+    
+}
+
+
+
 
